84d95ab1718bd26ba0e2e59750b29b335cdcb2c4
Sebastian Eibisch <seb.eibisch@icloud.com>
2024-01-17 19:20:03 +0100

    health endpoint

---
+++ src/main/java/de/seb/beer/server/rest/HealthRessource.java
@@ -0,0 +1,17 @@
+package de.seb.beer.server.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthRessource {
+
+    @GetMapping("health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok().build();
+    }
+
+}
